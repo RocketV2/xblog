d0d1d0fb58dfaa975bd5731d8177c9b5dc0b7f51
rocketv2 <2500392489@qq.com>
2017-06-26 22:10:15 +0800

    前端界面分页功能

--- app/routers/appRouters.js
+++ app/routers/appRouters.js
@@ -10,25 +10,51 @@ module.exports = router;
 
 // 首页 列表 取10篇文章
 router.get('/',function(req,res){
-	
+	// 分页功能 传递page参数
+	var query = querystring.parse( URL.parse(req.url).query );
+	var page  = query.page;
+
 	// 连接数据库 查询数据
 	mongo.Client.connect(mongo.URL,function(err,db){
 		if(err){
 			res.json({error:"database error!"});
 		}
+		// 跳过的条数
+		var skipCount = page?(page-1)*10:0;
+
 		var col = db.collection('articles');
-		col.find().limit(10).toArray(function(err,docs){
-			if(err){
-				res.json({error:"database error!"});
-			}
-			// markdown语法 文章内容需要转换
-			docs.forEach(function(val,index){
-				val.content = markdown.toHTML(val.content);
-			});
-			res.render('app_show_list',{
-				pageList:docs,
+
+		// 分页 10篇一页
+		col.count({},function(err,total){
+
+			col.find().skip(skipCount).limit(10).toArray(function(err,docs){
+				if(err){
+					res.json({error:"database error!"});
+				}
+				// 关闭数据库
+				db.close();
+				// 总页数
+				var pageTotalCount = ( (total%10==0)?parseInt(total/10):parseInt(total/10+1) );
+				// 查询页数超过真实总页数
+				if(page > pageTotalCount){
+					req.flash('error',tips.list_pagesOut);
+					return res.json({error:"error!"})
+				}
+
+				// markdown语法 文章内容需要转换
+				docs.forEach(function(val,index){
+					val.content = markdown.toHTML(val.content);
+				});
+
+				res.render('app_show_list',{
+					curKind:'',
+					pageList:docs,
+					pageCount:pageTotalCount,//总页数
+					curPage:page?page:1,// 当前页数
+				});
 			});
 		});
+		
 	});
 });
 
@@ -49,7 +75,6 @@ router.get('/blogs',function(req,res){
 			}
 			// markdown语法 文章内容需要转换
 			// docs[0].content = markdown.toHTML(docs[0].content);
-			console.log(docs)
 			res.render('app_show_page',{
 				pageData:docs[0],
 			});
@@ -60,6 +85,10 @@ router.get('/blogs',function(req,res){
 // 列表 根据分类获取文章列表
 router.get('/list',function(req,res){
 	var kind = querystring.parse(URL.parse(req.url).query).kind;
+	// 分页功能 传递page参数
+	var query = querystring.parse( URL.parse(req.url).query );
+	var page  = query.page;
+	
 
 	// 只允许以下参数
 	var allowKind = ['Javascript','ES6','Nodejs','MongoDB','Project'];
@@ -75,27 +104,69 @@ router.get('/list',function(req,res){
 		return res.redirect('/');
 	}
 
+	// // 连接数据库 查询数据
+	// mongo.Client.connect(mongo.URL,function(err,db){
+	// 	if(err){
+	// 		res.json({error:"database error!"});
+	// 	}
+	// 	var col = db.collection('articles');
+	// 	var selector = {'tags':kind};
+	// 	col.find(selector).limit(10).toArray(function(err,docs){
+	// 		if(err){
+	// 			res.json({error:"database error!"});
+	// 		}
+	// 		// markdown语法 文章内容需要转换
+	// 		docs.forEach(function(val,index){
+	// 			val.content = markdown.toHTML(val.content);
+	// 		});
+	// 		res.render('app_show_list',{
+	// 			pageList:docs,
+	// 		});
+	// 	});
+	// });
+
 	// 连接数据库 查询数据
 	mongo.Client.connect(mongo.URL,function(err,db){
 		if(err){
 			res.json({error:"database error!"});
 		}
+		// 跳过的条数
+		var skipCount = page?(page-1)*10:0;
+
 		var col = db.collection('articles');
 		var selector = {'tags':kind};
-		col.find(selector).limit(10).toArray(function(err,docs){
-			if(err){
-				res.json({error:"database error!"});
-			}
-			// markdown语法 文章内容需要转换
-			docs.forEach(function(val,index){
-				val.content = markdown.toHTML(val.content);
-			});
-			res.render('app_show_list',{
-				pageList:docs,
+		// 分页 10篇一页
+		col.count(selector,function(err,total){
+
+			col.find(selector).skip(skipCount).limit(10).toArray(function(err,docs){
+				if(err){
+					res.json({error:"database error!"});
+				}
+				// 关闭数据库
+				db.close();
+				// 总页数
+				var pageTotalCount = ( (total%10==0)?parseInt(total/10):parseInt(total/10+1) );
+				// 查询页数超过真实总页数
+				if(page > pageTotalCount){
+					req.flash('error',tips.list_pagesOut);
+					return res.json({error:"error!"})
+				}
+
+				// markdown语法 文章内容需要转换
+				docs.forEach(function(val,index){
+					val.content = markdown.toHTML(val.content);
+				});
+
+				res.render('app_show_list',{
+					curKind:kind,
+					pageList:docs,
+					pageCount:pageTotalCount,//总页数
+					curPage:page?page:1,// 当前页数
+				});
 			});
 		});
+		
 	});
-
 });
 
 
