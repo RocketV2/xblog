2f23f05be72c3e348ddea4021b306646f9a46751
rocketv2 <2500392489@qq.com>
2017-06-27 23:02:03 +0800

    update

--- app/js/app_index.js
+++ app/js/app_index.js
@@ -37,7 +37,7 @@ $(function(){
 				var name = val._id,
 					num  = val.num_total;
 				html += "<li class='docs-kinds-years-li'> " 
-							+"<a href='/list?kind=Javascript'>"+name+"&nbsp;("+num+")</a>"
+							+"<a href='/list?kind="+name+"'>"+name+"&nbsp;("+num+")</a>"
 						+"</li>";
 			});
 			$("#docs-kinds-years-ul").html(html);
