259626626e8bc344d67e7b618943c6a00bb9c4c2
rocketv2 <2500392489@qq.com>
2018-10-19 16:54:04 +0800

    update

--- confs/db.config.js
+++ confs/db.config.js
@@ -3,8 +3,8 @@
  * @type {Object}
  */
 module.exports = {
-	cookieSecret:'xblog',
-	db:'xblog',
-	host:'192.168.1.114',
-	port:12345
+	cookieSecret: 'xblog',
+	db: 'xblog',
+	host: '127.0.0.1',
+	port: 27017
 };
